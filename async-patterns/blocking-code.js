2ec9142ccd19c202d8748124e23ab39f332278b5
sam3690 <usamabinayoub@gmail.com>
2022-06-10 03:19:19 +0500

    Updating blocking code

--- async-patterns/blocking-code.js
+++ async-patterns/blocking-code.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     if (req.url ==='/') {
-        res.end('Home Page');
+        res.end('Home Page!');
         
     }
     else if(req.url === '/about'){
         // BLOCKING CODE !!!!
         for (let i = 0; i <100 ; i++) {
             for (let j = 0; j < 100; j++) {
-                
                 console.log(`${i} ${j}`);
                 
             } 
